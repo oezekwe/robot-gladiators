fc1135da7d767d859ef92cb31af432df2c0dae3a
Obinna Ezekwe <ozekwe@gmail.com>
2020-10-06 01:39:21 -0500

    game taking shape

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,37 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    // Alert users that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
+    
+    enemyHealth-=playerAttack;
+  
+    console.log(playerName+ " attacked "+enemyName+". "+enemyName+" now has "+enemyHealth+" health remaining.");
+    if(enemyHealth<=0){
+        window.alert(enemyName+ " has died!");
+    }
+    else{
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+    
+    playerHealth-=enemyAttack;
+    if(playerHealth<=0){
+        window.alert(playerName+ " has died!");
+    }
+    else{
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
+  
+    console.log(enemyName+ " attacked "+playerName+". "+playerName+" now has "+playerHealth+" health remaining.");
+  };
+
+fight();
