4a8208240504925087e5dcec04f2503460ce6332
Obinna Ezekwe <ozekwe@gmail.com>
2020-10-07 01:11:36 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,8 +7,14 @@ var playerAttack = 10;
 console.log(playerName, playerAttack, playerHealth);
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
-var enemyHealth = 50;
+var enemyHealth = 50
 var enemyAttack = 12;
+
+var randomNumber= function(min, max){
+    var value= Math.floor(Math.random() * (max-min+1)+min);
+
+    return value;
+};
 
 var fight = function(enemyName) {
     while(playerHealth > 0 && enemyHealth > 0){
@@ -18,12 +24,13 @@ var fight = function(enemyName) {
     
             if(confirmSkip){
                 window.alert(playerName + " has decided to skip this fight. Goodbye!");
-                playerMoney-=10;
+                playerMoney= Math.max(0, playerMoney-10);
                 console.log("playerMoney", playerMoney);
                 break;
             }
         }
-        enemyHealth-=playerAttack;
+        var damage = randomNumber(playerAttack - 3, playerAttack);
+        enemyHealth = Math.max(0, enemyHealth - damage);
         console.log(playerName+ " attacked "+enemyName+". "+enemyName+" now has "+enemyHealth+" health remaining.");
 
         if(enemyHealth<=0){
@@ -35,7 +42,8 @@ var fight = function(enemyName) {
             window.alert(enemyName + " still has " + enemyHealth + " health left.");
         }
 
-        playerHealth-=enemyAttack;
+        var damage = randomNumber(enemyAttack - 3, enemyAttack);
+        playerHealth = Math.max(0, playerHealth - damage);
         console.log(enemyName+ " attacked "+playerName+". "+playerName+" now has "+playerHealth+" health remaining.");
 
         if(playerHealth<=0){
@@ -56,7 +64,7 @@ var fight = function(enemyName) {
         if (playerHealth > 0) {
             window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
             var pickedEnemyName = enemyNames[i];
-            enemyHealth = 50;
+            enemyHealth = randomNumber(40, 60);
             fight(pickedEnemyName);
             if(playerHealth > 0 && i < enemyNames.length-1){
                 var storeConfirm= window.confirm("The fight is over, visit the store before the next round?");
