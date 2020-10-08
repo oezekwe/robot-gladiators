caaf4f09935950d39b8f81d27ca4523bb5b55989
Obinna Ezekwe <ozekwe@gmail.com>
2020-10-07 23:44:59 -0500

    Fixed the prompt bug in fight()

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,18 +6,35 @@ var randomNumber= function(min, max){
     return value;
 };
 
+var fightOrSkip= function(){
+    var promptFight= window.prompt('Would you like FIGHT OR SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+
+    if (promptFight === "" || promptFight === null) {
+        window.alert("You need to provide a valid answer! Please try again.");
+        return fightOrSkip();
+    }
+
+    promptFight = promptFight.toLowerCase();
+    if (promptFight === "skip") {
+        // confirm user wants to skip
+        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+    
+        // if yes (true), leave fight
+        if (confirmSkip) {
+          window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
+          // subtract money from playerMoney for skipping
+          playerInfo.money = Math.max(0, playerInfo.money - 10);
+          
+          return true;
+        }
+        return false;
+    }
+};
+
 var fight = function(enemy) {
     while(playerInfo.health > 0 && enemy.health > 0){
-        var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
-        if(promptFight === "skip" || promptFight === "SKIP"){
-            var confirmSkip= window.confirm("Are you sure you'd like to quit?");
-    
-            if(confirmSkip){
-                window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
-                playerInfo.money= Math.max(0, playerInfo.money-10);
-                console.log("playerInfo.money", playerInfo.money);
-                break;
-            }
+        if(fightOrSkip()){
+            break;
         }
         var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
         enemy.health = Math.max(0, enemy.health - damage);
